c7ca45ac763055d4b6234d9b4da5ff8abf96fc05
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-05-18 16:17:04 +0200

    iniziato lo style

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
+
 
 class HomeController extends Controller
 {
-    //
+  public function index()
+  {
+    $title = 'Home';
+    $movies = Movie::all();
+    return view('home', compact('movies'))->with('title', $title);
+  }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,25 @@
 
 
 <main id="home">
-  home
+
+  <div class="container flex">
+    @foreach ($movies as $movie)
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <h1>{{$movie['title']}}</h1>
+          </div>
+          <div class="flip-card-back">
+            <h1>{{$movie['original_title']}}</h1>
+            <p>{{$movie['nationality']}}</p>
+            <p>{{$movie['date']}}</p>
+            <p>{{$movie['vote']}}</p>
+          </div>
+        </div>
+      </div>
+    @endforeach
+  </div>
+
 </main>
 
 
